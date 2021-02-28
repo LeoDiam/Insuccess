45398bee4d99b2ced224008b3a51d3a2b4d160d0
Λεωνιδας Διαμαντόπουλος <t8190243@aueb.gr>
2021-03-01 00:58:11 +0200

    Add files via upload
    Succesfully adding apache poi and controlling the excel's data in order to put them in the database using jdbc.

---
+++ InSuccess/src/Insuccess/app/main/ReadExcelDemo.java
@@ -0,0 +1,73 @@
+package Insuccess.app.main;
+
+import Insuccess.app.data.DaoImpl;
+import Insuccess.app.manager.DBConnectionManager;
+import Insuccess.app.model.Questions;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadExcelDemo {
+  static int counter = 0;
+    static int count = 99;
+    public static void main(String[] args) throws IOException {
+//obtaining input bytes from a file
+        FileInputStream fis = new FileInputStream("C:\\Users\\leoni\\OneDrive\\Υπολογιστής\\practice.xlsx");
+//creating workbook instance that refers to .xls file
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+//creating a Sheet object to retrieve the object
+        XSSFSheet sheet = wb.getSheetAt(0);
+//evaluating cell type
+        FormulaEvaluator formulaEvaluator = wb.getCreationHelper().createFormulaEvaluator();
+
+        for (Row row : sheet)     //iteration over row using for each loop
+        {
+
+
+            for (Cell cell : row)    //iteration over cell using for each loop
+            {
+                switch (formulaEvaluator.evaluateInCell(cell).getCellType()) {
+                    case Cell.CELL_TYPE_NUMERIC:   //field that represents numeric cell type
+//getting the value of the cell as a number
+
+                       // System.out.print(cell.getNumericCellValue() + "\t\t");
+                        break;
+                    case Cell.CELL_TYPE_STRING:    //field that represents string cell type
+//getting the value of the cell as a string
+
+                        if(counter > 1071 && counter <= 1296) {
+                            System.out.println(wb.getSheetAt(0).getRow(counter).getCell(0)); //id
+//                            System.out.println(wb.getSheetAt(0).getRow(counter).getCell(3)); //ekfonisi
+//                            System.out.print("  "+wb.getSheetAt(0).getRow(counter).getCell(1)); //svsth apantisi
+//                            System.out.print("  "+wb.getSheetAt(0).getRow(counter+1).getCell(3));//apant1
+//                            System.out.print("  "+wb.getSheetAt(0).getRow(counter+2).getCell(3));//apant2
+//                            System.out.print("  "+wb.getSheetAt(0).getRow(counter+3).getCell(3));//apant3
+//                            System.out.print("  "+wb.getSheetAt(0).getRow(counter+4).getCell(3));//apant4
+                            Questions q = new Questions(++count,
+                                    wb.getSheetAt(0).getRow(counter).getCell(3),
+                                    wb.getSheetAt(0).getRow(counter+1).getCell(3),
+                                    wb.getSheetAt(0).getRow(counter+2).getCell(3),
+                                    wb.getSheetAt(0).getRow(counter+3).getCell(3),
+                                    wb.getSheetAt(0).getRow(counter+4).getCell(3),
+                                    wb.getSheetAt(0).getRow(counter).getCell(1));
+                            DBConnectionManager manager = new DBConnectionManager();
+                            DaoImpl impl = new DaoImpl(manager);
+                              impl.insertQuestions(q);
+                        }
+
+                        counter +=5;
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
+
